afa0faf3b4ded2a9eda860e58102647383de7760
Beybaris <bejbaryszappar790@gmail.com>
2026-06-16 02:24:34 +0500

    Some changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from backend.app.router.user import router as user_router
 from backend.app.router.customer import router as customer_router
+from backend.app.router.products import router as product_router
+from backend.app.router.categories import router as category_router
 app = FastAPI()
 
 
 app.include_router(user_router)
 app.include_router(customer_router)
+app.include_router(product_router)
+app.include_router(category_router)
